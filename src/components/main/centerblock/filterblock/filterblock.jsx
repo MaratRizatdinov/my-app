94c2626f36f5d17ba84fdf0b8889cc7189e78c1b
MaratRizatdinov <maratrizatdinov91@gmail.com>
2023-08-19 22:56:56 +0300

    Выпадающий список авторов из массива

--- src/components/main/centerblock/filterblock/filterblock.jsx
+++ src/components/main/centerblock/filterblock/filterblock.jsx
@@ -4,6 +4,7 @@ import FilterByAuthor from './filterbyauthor/filterbyauthor';
 import FilterByYear from './filterbyyear/filterbyyear';
 import FilterByGenre from './filterbygenre/filterbygenre';
 
+
 function Filterblock(){
     const [activeIndex, setActiveIndex] = useState(0);
     return <><h2 className="centerblock__h2">Треки</h2>
@@ -12,7 +13,7 @@ function Filterblock(){
       <FilterByAuthor isActive={activeIndex} onShow={() => activeIndex!==1 ? setActiveIndex(1):setActiveIndex(0)}/>
       <FilterByYear isActive={activeIndex} onShow={() => activeIndex!==2 ? setActiveIndex(2):setActiveIndex(0)}/>   
       <FilterByGenre isActive={activeIndex} onShow={() => activeIndex!==3 ? setActiveIndex(3):setActiveIndex(0)}/>         
-      console.log(activeIndex);
+      
     </div></>;
 }
 
--- src/components/main/centerblock/filterblock/filterbyauthor/filterbyauthor.jsx
+++ src/components/main/centerblock/filterblock/filterbyauthor/filterbyauthor.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './filterbyauthor.css';
+import { workArray } from '../../../../../workarray';
 
 function FilterByAuthor({isActive, onShow }){
-    // console.log(isActive);
+
+    const array=workArray();
+    const newArr =array.map((key)=>{
+        return key['track author']});    
+    
+    const UniqueArrayOfAuthor=[...new Set(newArr.sort())];
+    
+    const listOfItems = UniqueArrayOfAuthor.map((key)=>{
+        return <a href='#' className='filter__author-items'key={key}>{key}</a>
+    });
+    
+        
+    
+
+    
     const openClass = isActive===1 ? "filter__button button-author _btn-text _btn-text-open  " : "filter__button button-author _btn-text";
     return   <div className='filter__author-block'>
                 <div className={openClass} onClick={onShow}>
@@ -10,21 +25,10 @@ function FilterByAuthor({isActive, onShow }){
                 </div>
                 { isActive!==1 ? "": <div className='filter__author-list'>
                     <div className='filter__author-container'>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>
-                        <a href='#' className='filter__author-items'>DDDD DDDD</a>                        
+                        {listOfItems} 
                     </div>                    
-                </div>}
-                
-            </div>
-            
+                </div>}                
+            </div>           
             
 }
 
