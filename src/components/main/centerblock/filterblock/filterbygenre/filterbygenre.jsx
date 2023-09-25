07825a4b94f9d269bac280c55d20d7bfac7c698a
MaratRizatdinov <maratrizatdinov91@gmail.com>
2023-09-25 12:29:30 +0300

    README

--- src/components/main/centerblock/filterblock/filterbygenre/filterbygenre.jsx
+++ src/components/main/centerblock/filterblock/filterbygenre/filterbygenre.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from 'react'
 import * as S from './filterbygenre.style'
 import { useSelector } from 'react-redux'
 
-
-
-function FilterByGenre({isActive, onShow }){
-    const array = useSelector((s) => s.state.playlist)    
+function FilterByGenre({ isActive, onShow }) {
+    const array = useSelector((s) => s.state.playlist)
     const newArr = array.map((key) => {
         return key.genre
     })
     const UniqueArrayOfGenre = [...new Set(newArr.sort())]
-    console.log(UniqueArrayOfGenre)
+
     const listOfItems = UniqueArrayOfGenre.map((key) => {
         return (
             <S.FilterGenreItems href="#" key={key}>
@@ -18,20 +16,23 @@ function FilterByGenre({isActive, onShow }){
             </S.FilterGenreItems>
         )
     })
-    
-    
-    return   <S.FilterGenreBlock>
-                <S.FilterButtonGenre primary={isActive} onClick={onShow}>
-                 жанру
-                </S.FilterButtonGenre>
-                { isActive!==3 ? "": <S.FilterGenreList>
+
+    return (
+        <S.FilterGenreBlock>
+            <S.FilterButtonGenre primary={isActive} onClick={onShow}>
+                жанру
+            </S.FilterButtonGenre>
+            {isActive !== 3 ? (
+                ''
+            ) : (
+                <S.FilterGenreList>
                     <S.FilterGenreContainer>
-                        {listOfItems}                        
-                    </S.FilterGenreContainer>                    
-                </S.FilterGenreList>}                
-            </S.FilterGenreBlock>
-            
-            
+                        {listOfItems}
+                    </S.FilterGenreContainer>
+                </S.FilterGenreList>
+            )}
+        </S.FilterGenreBlock>
+    )
 }
 
-export default FilterByGenre;
+export default FilterByGenre
