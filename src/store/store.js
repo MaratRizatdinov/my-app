f67068dc8dcb96711155786bfe875a89d3c65976
MaratRizatdinov <maratrizatdinov91@gmail.com>
2023-09-29 15:40:03 +0300

    middleware thunk in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TrackReducer } from './reducers/TrackReducer'
+import thunk from 'redux-thunk';
 
 const store = configureStore({
     reducer: {
         state: TrackReducer,
     },
+    middleware:[thunk],
 })
 
 export default store;
