8e22af2f1b2e163a08e79d7d3cd1186107fb1e5b
MaratRizatdinov <maratrizatdinov91@gmail.com>
2023-10-11 20:21:34 +0300

    Реализован лайк в баре

--- src/components/bar/TrackPlayLikeDiz/trackplaylikediz.jsx
+++ src/components/bar/TrackPlayLikeDiz/trackplaylikediz.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import * as S from './trackplaylikediz.style';
+import React from 'react'
+import * as S from './trackplaylikediz.style'
+import { useSelector } from 'react-redux'
+import {
+    useGetAllFavoritesQuery,
+    useAddFavoriteTrackMutation,
+    useDeleteFavoriteTrackMutation,
+} from '../../../store/services/favorite'
 
+function TrackPlayLikeDiz() {
+    const token = useSelector((s) => s.state.accessToken)
+    const currentTrack = useSelector((s) => s.state.currentTrack)
+    const { data: favoritesPlaylist } = useGetAllFavoritesQuery(token)
+    const [addFavorite] = useAddFavoriteTrackMutation()
+    const [deleteFavorite] = useDeleteFavoriteTrackMutation()
 
-function TrackPlayLikeDiz(){
-    return <S.TrackPlayLikeDis>
-    
-    <S.TrackPlayLike className="_btn-icon">
-      <S.TrackPlayLikeSvg alt="like">
-        <use xlinkHref="../img/icon/sprite.svg#icon-like"></use>
-      </S.TrackPlayLikeSvg>
-    </S.TrackPlayLike>
-    
-    <S.TrackPlayDis className="_btn-icon">
-      <S.TrackPlayDisSvg alt="dislike">
-        <use
-          xlinkHref="../img/icon/sprite.svg#icon-dislike"
-        ></use>
-      </S.TrackPlayDisSvg>
-    </S.TrackPlayDis>
-  </S.TrackPlayLikeDis>;
+    const favoriteArrayByID = favoritesPlaylist.map((elem) => elem.id)
+    const likedStatus = favoriteArrayByID.includes(currentTrack.id)
+
+    function handleClick() {
+        likedStatus
+            ? deleteFavorite({ id: currentTrack.id, accessToken: token })
+            : addFavorite({ id: currentTrack.id, accessToken: token })
+    }
+
+    return (
+        <S.TrackPlayLikeDis>
+            <S.TrackPlayLike className="_btn-icon" onClick={handleClick}>
+                <S.TrackPlayLikeSvg alt="like">
+                    <use xlinkHref="../img/icon/sprite.svg#icon-like"></use>
+                    {likedStatus ? (
+                        <use xlinkHref="../img/icon/sprite.svg#icon-likeclicked"></use>
+                    ) : (
+                        <use xlinkHref="../img/icon/sprite.svg#icon-like"></use>
+                    )}
+                </S.TrackPlayLikeSvg>
+            </S.TrackPlayLike>
+        </S.TrackPlayLikeDis>
+    )
 }
 
-export default TrackPlayLikeDiz;
-
+export default TrackPlayLikeDiz
